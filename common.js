13f714f3dd2daf158c41aec6c5a8c31beeca12bb
Hutlaw <154370020+Hutlaw@users.noreply.github.com>
2023-12-19 23:12:49 -0500

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,7 @@
 document.write(`
-  <footer>
-    This is the shared footer content.
-  </footer>
+    <footer>
+    <sub>-Hutlaw from discord  |   </sub>
+  </body>
+  <a class="comment_button" href="https://github.com/hutlaw/hutlaw.github.io/discussions/1">Comment</a>
+</footer>
 `);

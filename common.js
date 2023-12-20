494149e695ea8618c7169a17c4c09f7545bb13ee
Hutlaw <154370020+Hutlaw@users.noreply.github.com>
2023-12-19 23:17:57 -0500

    Delete common.js

--- common.js
+++
@@ -1,7 +0,0 @@
-document.write(`
-    <footer>
-    <sub>-Hutlaw from discord  |   </sub>
-  </body>
-  <a class="comment_button" href="https://github.com/hutlaw/hutlaw.github.io/discussions/1">Comment</a>
-</footer>
-`);
